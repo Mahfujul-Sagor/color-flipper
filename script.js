5094126eb46e20ce95e494cef1a9f545e74e294e
Mahfujul-Sagor <mahfujulsagor@gmail.com>
2024-02-09 21:36:08 +0600

    finished the js file

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ let white = document.querySelector('.white');
 let black = document.querySelector('.black');
 let random = document.querySelector('.random');
 
-
+// setting color
 function setColor(color) {
   body.style.backgroundColor = color;
 };
 
+// action on button click
 green.addEventListener('click', ()=> {
   setColor('green');
 });
@@ -22,8 +23,15 @@ white.addEventListener('click', ()=> {
 black.addEventListener('click', ()=> {
   setColor('black');
 });
+random.addEventListener('click', ()=> {
+  setColor(randomColor());
+});
 
-
+// generating a random color
 function randomColor() {
-  
+  const red = Math.round(Math.random() * 255);
+  const green = Math.round(Math.random() * 255);
+  const blue = Math.round(Math.random() * 255);
+  const color = `rgb(${red}, ${green}, ${blue})`;
+  return color;
 };
